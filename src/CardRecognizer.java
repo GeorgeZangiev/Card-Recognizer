421e133fec9be144595989331053246213071d4c
Artur Zangiev <zangiev@gmail.com>
2022-07-26 01:40:14 +0100

    first outline of image

--- src/CardRecognizer.java
+++ src/CardRecognizer.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class CardRecognizer {
-    private HashMap<BufferedImage, String> cardsMap = new HashMap<>();
+    private HashMap<String, String> cardsMap = new HashMap<>();
 
     public CardRecognizer() throws IOException {
         this.loadCards();
@@ -18,7 +18,7 @@ public class CardRecognizer {
             BufferedImage image = ImageIO.read(picture);
             String fileName = picture.getName();
             String fileWithoutExtention = fileName.split("[.]")[0];
-            System.out.println(fileWithoutExtention);
+            // System.out.println(fileWithoutExtention);
             String[] cardAbbriviations = fileWithoutExtention.split("(?<=\\G(\\d{1,2}|[A-Z])[a-z])");
             int x = 150;
             for (String cardAbbriviation : cardAbbriviations) {
@@ -27,14 +27,25 @@ public class CardRecognizer {
                 // File outputfile = new File(cardAbbriviation + ".png");
                 // ImageIO.write(croppedCard, "png", outputfile);
                 x = x + 70;
-                cardsMap.put(croppedCard, cardAbbriviation);
-                System.out.println(cardsMap.get(croppedCard));
+                cardsMap.put(image.getRaster().getDataBuffer().getData(), cardAbbriviation);
+                // System.out.println(cardsMap.get(croppedCard));
             }
         }
     }
 
-    // private void ext() {
-        
-    // }
+    public void readCards() throws IOException {
+        String directoryPath = "/Users/arturzangiev/Projects/Card-Recognizer/learning_imgs";
+        File[] directoryListing = new File(directoryPath).listFiles();
+        for (File picture: directoryListing) { 
+            BufferedImage image = ImageIO.read(picture);
+            int x = 150;
+            for (int i = 0; i < 5; i++) {
+                BufferedImage croppedCard = image.getSubimage(x, 590, 51, 76);
+                String cardAbbriviation = cardsMap.get(image);
+                System.out.println(cardAbbriviation);
+                x = x + 70;
+            }
+        }
+    }
 
 }
