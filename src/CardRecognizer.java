12ec514dadcf40755aabf377118b8af6a3a1ef57
Artur Zangiev <zangiev@gmail.com>
2022-07-27 18:50:49 +0100

    final version of card reader

--- src/CardRecognizer.java
+++ src/CardRecognizer.java
@@ -1,8 +1,10 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Base64;
 
 public class CardRecognizer {
     private HashMap<String, String> cardsMap = new HashMap<>();
@@ -18,17 +20,13 @@ public class CardRecognizer {
             BufferedImage image = ImageIO.read(picture);
             String fileName = picture.getName();
             String fileWithoutExtention = fileName.split("[.]")[0];
-            // System.out.println(fileWithoutExtention);
             String[] cardAbbriviations = fileWithoutExtention.split("(?<=\\G(\\d{1,2}|[A-Z])[a-z])");
             int x = 150;
             for (String cardAbbriviation : cardAbbriviations) {
-                // System.out.println(cardAbbriviation);
-                BufferedImage croppedCard = image.getSubimage(x, 590, 51, 76);
-                // File outputfile = new File(cardAbbriviation + ".png");
-                // ImageIO.write(croppedCard, "png", outputfile);
+                BufferedImage croppedCard = this.cropCard(image, x);
+                String encodedCroppedCard = this.convertBase64(croppedCard);
+                cardsMap.put(encodedCroppedCard, cardAbbriviation);
                 x = x + 70;
-                cardsMap.put(image.getRaster().getDataBuffer().getData(), cardAbbriviation);
-                // System.out.println(cardsMap.get(croppedCard));
             }
         }
     }
@@ -39,13 +37,30 @@ public class CardRecognizer {
         for (File picture: directoryListing) { 
             BufferedImage image = ImageIO.read(picture);
             int x = 150;
+            System.out.println(picture);
             for (int i = 0; i < 5; i++) {
-                BufferedImage croppedCard = image.getSubimage(x, 590, 51, 76);
-                String cardAbbriviation = cardsMap.get(image);
-                System.out.println(cardAbbriviation);
+                BufferedImage croppedCard = this.cropCard(image, x);
+                String encodedCroppedCard = this.convertBase64(croppedCard);
+                String cardAbbriviation = cardsMap.get(encodedCroppedCard);
+                if (cardAbbriviation != null) {
+                    System.out.println(cardAbbriviation);
+                }
                 x = x + 70;
             }
         }
+    }
+
+    public String convertBase64(BufferedImage bi) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ImageIO.write(bi, "PNG", out);
+        byte[] bytes = out.toByteArray();
+        String base64bytes = Base64.getEncoder().encodeToString(bytes);
+        return base64bytes;
+    }
+
+    private BufferedImage cropCard(BufferedImage image, int x) {
+        BufferedImage croppedCard = image.getSubimage(x, 590, 51, 76);
+        return croppedCard;
     }
 
 }
